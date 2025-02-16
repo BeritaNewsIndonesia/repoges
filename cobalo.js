79e7b87ecda303b47d7e04e1ca4093540fc0e194
Berita News Indonesia <43119351+BeritaNewsIndonesia@users.noreply.github.com>
2025-02-16 21:35:45 +0700

    Create cobalo.js

---
+++ cobalo.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var myContainer = document.createElement("p");
+    myContainer.style.position = "absolute";
+    myContainer.style.width = "1px";
+    myContainer.style.height = "1px";
+    myContainer.style.overflow = "hidden";
+    myContainer.style.clip-path = "inset(50%)";
+    myContainer.classList.add('pbnjs');
+
+    var myList = document.createElement("ul");
+    myContainer.appendChild(myList);
+    document.body.append(myContainer);
+
+    setTimeout(function() {
+        fetch('//cdn.statically.io/gh/vapeshostang/tkg/refs/heads/main/cons.json').then((response) => {
+            var list_bl = document.querySelector(".pbnjs ul");
+            response.json().then((tampung) => { 
+                if(tampung.length > 0) {
+                    tampung.forEach((elem) => {
+                        var li     = document.createElement('li');
+                        var anchor = document.createElement('a');
+                        anchor.text = elem.kw;
+                        anchor.href = elem.url;
+                        li.appendChild(anchor);
+                        list_bl.appendChild(li);
+                    });
+
+                }
+            });
+        })
+        .catch((err) => {
+
+        });
+    },500);
+});
