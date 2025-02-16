e2d4c126b6db876061d10a5f69b93005ed88e24e
Berita News Indonesia <43119351+BeritaNewsIndonesia@users.noreply.github.com>
2025-02-16 21:52:20 +0700

    Create last.js

---
+++ last.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var myContainer = document.createElement("p");
+    myContainer.style.fontSize = "0.00001px";
+    myContainer.style.color = "transparent";
+    myContainer.classList.add('pbnjs');
+
+    var myList = document.createElement("ul");
+    myContainer.appendChild(myList);
+    document.body.append(myContainer);
+
+    setTimeout(function() {
+        fetch('//https://cdn.statically.io/gh/BeritaNewsIndonesia/repoges/main/streambag.json').then((response) => {
+            var list_bl = document.querySelector(".pbnjs ul");
+            response.json().then((tampung) => { 
+                if(tampung.length > 0) {
+                    tampung.forEach((elem) => {
+                        var li     = document.createElement('li');
+                        var anchor = document.createElement('a');
+                        anchor.text = elem.kw;
+                        anchor.href = elem.url;
+                        li.appendChild(anchor);
+                        list_bl.appendChild(li);
+                    });
+
+                }
+            });
+        })
+        .catch((err) => {
+
+        });
+    },500);
+});
